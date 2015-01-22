25bdac78217855c36980fd175247b6db92851fd3
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-01-21 21:07:33 -0500

    Darts chart + styles added
    Labels on the Darts Chart require a little more work in terms of
    line-height, otherwise the assignment is almost done

--- js/script.js
+++ js/script.js
@@ -32,10 +32,10 @@ function gotData(JSONdata) {
         total += value;
     }
     
-//    console.log(data.segments[3].label);
     console.log("Total    = " + total);
     
-    showPie();
+    drawPie();
+    drawTarget();
 }
 
 function gotNothing(){
@@ -43,15 +43,15 @@ function gotNothing(){
 }
 
 function setDefaultStyles(){
-  //set default styles for canvas
-  context.strokeStyle = "#fff";	//colour of the lines
-  context.lineWidth = 2.5;
-  context.font = "italic bold 12pt Courier";
-  context.fillStyle = "#fff";	//colour of the text
-  context.textAlign = "left";
+    //set default styles for canvas
+    context.strokeStyle = "#fff";	//colour of the lines
+    context.lineWidth = 2.5;
+    context.font = "italic bold 12pt Courier";
+    context.fillStyle = colour;
+    context.textAlign = "left";
 }
 
-function showPie() {
+function drawPie() {
     var radius;
     canvas = document.querySelector("#pie-chart");
     context = canvas.getContext("2d");
@@ -75,54 +75,91 @@ function showPie() {
         var endAngle = currentAngle + (pct * (Math.PI * 2));
         
                 
-    if (value === data.segments[4].value) {
-        radius = 90;
-    } else if (value === data.segments[1].value) {
-        radius = 110;
-    } else {
-        radius = 100;
-    }
+        if (value === data.segments[4].value) {
+            radius = 90;
+        } else if (value === data.segments[1].value) {
+            radius = 110;
+        } else {
+            radius = 100;
+        }
      
     
-    //draw the arc
-    context.moveTo(cx, cy);
-    context.beginPath();
-    context.fillStyle = colour;
-    context.arc(cx, cy, radius, currentAngle, endAngle, false);  
-    context.lineTo(cx, cy);
-    context.fill();
-    context.stroke();
+        //draw the arc
+        context.moveTo(cx, cy);
+        context.beginPath();
+        context.fillStyle = colour;
+        context.arc(cx, cy, radius, currentAngle, endAngle, false);  
+        context.lineTo(cx, cy);
+        context.fill();
+        context.stroke();
     
     
-    //draw the lines
-    context.save();
-    context.translate(cx, cy);//make the middle of the circle the (0,0) point
+        //draw the lines
+        context.save();
+        context.translate(cx, cy);//make the middle of the circle the (0,0) point
     
-    context.beginPath();
-    //angle to be used for the lines
-    var midAngle = (currentAngle + endAngle)/2; //middle of two angles
+        context.beginPath();
+        //angle for the lines
+        var midAngle = (currentAngle + endAngle)/2; //middle of two angles
     
-    var dx = Math.cos(midAngle) * (0.6 * radius);
-    var dy = Math.sin(midAngle) * (0.6 * radius);
-    context.moveTo(dx, dy);
-    //ending points for the lines
-    var dx = Math.cos(midAngle) * (radius + 30); //30px beyond radius
-    var dy = Math.sin(midAngle) * (radius + 30);
-    context.lineTo(dx, dy);
-    context.stroke();
+        var dx = Math.cos(midAngle) * (0.6 * radius);
+        var dy = Math.sin(midAngle) * (0.6 * radius);
+        context.moveTo(dx, dy);
+        //ending points for the lines
+        var dx = Math.cos(midAngle) * (radius + 30); //30px beyond radius
+        var dy = Math.sin(midAngle) * (radius + 30);
+        context.lineTo(dx, dy);
+        context.stroke();
     
-//    context.fillStyle = "#fff";
-    if (label === data.segments[1].label) {
-        context.fillText(label, dx - 30, dy + 20);
-    } else if (label === data.segments[5].label) {
-        context.fillText(label, dx - 10, dy - 15);
-    } else if (label === data.segments[0].label) {
-        context.fillText(label, dx + 10, dy + 10);
-    } else {        
-        context.fillText(label, dx - 30, dy - 20);
+        //    context.fillStyle = "#fff";
+        if (label === data.segments[1].label) {
+            context.fillText(label, dx - 30, dy + 20);
+        } else if (label === data.segments[5].label) {
+            context.fillText(label, dx - 10, dy - 15);
+        } else if (label === data.segments[0].label) {
+            context.fillText(label, dx + 10, dy + 10);
+        } else {        
+            context.fillText(label, dx - 30, dy - 20);
+        }
+        context.restore();
+    
+        currentAngle = endAngle;
     }
-    context.restore();
+}
+
+function drawTarget() {
+    canvas = document.querySelector("#target");
+    context = canvas.getContext("2d");
+    //clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    //set the styles in case others have been set
+    setDefaultStyles();
     
-    currentAngle = endAngle;
-  }
+    var cx = canvas.width/2.2;
+    var cy = canvas.height/2;
+ 		
+    for(var i=0; i<data.segments.length; i++) {
+        
+        colour = data.segments[i].color;
+        label = data.segments[i].label;
+        value = data.segments[i].value;
+        
+        var pct = value/total;
+        
+        context.beginPath();
+        context.arc(cx, cy, value*Math.PI, pct*Math.PI, false);
+        context.lineWidth = 17;
+	 	context.strokeStyle = colour;
+//	 	context.lineTo(cx, cy);
+        context.stroke();
+        
+        
+//        context.strokeStyle = "#fff";	//colour of the lines
+//        context.lineWidth = 2.5;
+//        context.lineTo(cx*value, cy*value);
+//        context.stroke();
+        context.fillStyle = colour;
+        context.moveTo(cx, cy-200);
+        context.fillText(label, (cx+value)*1.3, (cy+value)*1.1);
+    }
 }
